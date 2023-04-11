db61949232f3ed7b15716f8059358b92236a5a93
federicojoaquin <federicojoaquing01@gmail.com>
2023-04-11 12:07:38 -0300

    11/04

--- app/components/Section1.js
+++ app/components/Section1.js
@@ -20,7 +20,7 @@ function Section1() {
             <h1 className={`titleAboutMe`}><b>Sobre mí</b></h1>
             <p className={`${alata.className} paragraph`} style={{marginTop: '1rem', lineHeight: '2rem'}}><b>Desarrollador web autodidacta</b> y estudiante de Ingeniería en Sistemas.  
                 Cuento con tres años aprendiendo diversas tecologías y he desempeñando <b>rol de desarrollo y soporte</b> en un sistema <b>ERP</b>, en donde me familiaricé con <b>SQL. </b> 
-                  Actualemente, me encuentro mejorando mis habilidades en <b>desarrollo web</b>, desde hace dos años. 
+                  Actualemente, me encuentro mejorando mis habilidades en <b>desarrollo web</b> desde hace dos años. 
                   Desarrollo <b>páginas web estáticas</b> a medida, además implementé nuevas prácticas para el desarrollo de aplicaciones <b>CRUD</b> con tecnologías como <b>REACT.JS, NEXT.JS y NODE.JS</b></p>. 
                 {/* <Link class="btnNeon" target="_blank" href={'../Pdf'} id='download' style={{marginTop: '1rem'}}><b>Descargar CV</b> <i class="bi bi-arrow-down"></i></Link> */}
                 <a className="btnNeon" href='/pdfs/FedericoGarcia.pdf' id="enlaceDescargarPdf" download="FedericoGarcia.pdf"><b>Descargar CV</b> <i class="bi bi-arrow-down"></i></a>
