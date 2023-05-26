7efb76a3f0549649e78a30116889b8b668f7a1c8
federicojoaquin <federicojoaquing01@gmail.com>
2023-05-26 11:32:51 -0300

    Mas proyectos width corregido

--- app/components/Proyects.js
+++ app/components/Proyects.js
@@ -2,7 +2,7 @@
 
 import {React, useState, useEffect, useRef} from 'react'
 import Proyect from './Proyect'
-import proyectImage from '../assets/code_think.svg'
+// import proyectImage from '../assets/code_think.svg'
 import './Styles/Proyect.css'
 
 function Proyects() {
@@ -28,7 +28,7 @@ function Proyects() {
     <div className={`proysContainer ${myElementIsVisible ? 'proysVis' : 'proysNotVis'}`} ref={myRef}>
       {/* Map */}
 
-      <Proyect proy={proyectImage} 
+      <Proyect proy={"https://i.ibb.co/61bx4hX/Mauri-app.jpg"} 
       titleProy='Mauricio Pereyra blog'
       descProy={`Página web estática de presentación de músico Mauricio Pereyra, con sus álbumes, biografía y contacto. Plantilla HTML editada, Next.js y deploy en Vercel.`}
       linkProy='https://gitlab.com/federicojoaquin/mauriciopereyra-nextjs-blog'
@@ -36,7 +36,7 @@ function Proyects() {
       textBtn1='Visitar'
       btn2={true} />
 
-      <Proyect proy={proyectImage} 
+      <Proyect proy={"https://i.ibb.co/XLY4YK7/Gastos-app.jpg"} 
       titleProy='Módulo gastos'
       descProy={`CRUD para carga de gastos, front-end con React, Next.js, Bootstrap. Back-end con Node.js, Express y PostgreSQL. Deploys: Front en Vercel, API en Render.com y base de datos en Neon.`}
       linkProy='https://github.com/federicojoaquin/Nextjs-gastos'
@@ -48,14 +48,14 @@ function Proyects() {
       link2={'https://nextjs-gastos.vercel.app/'}
       />
 
-      <Proyect proy={proyectImage} 
+      <Proyect proy={"https://i.ibb.co/KWJdr26/Administrador.jpg"} 
       titleProy='Administrador Personal' 
       descProy={`En desarrollo, gestor de gastos, finanzas, y tiempos para uso personal. Tecnologías: React, Node.Js, Express y PostgreSql.`}
       linkProy='https://github.com/federicojoaquin/Administrador-Personal'
       textBtn1='Visitar'
       /> 
       
-      <Proyect proy={proyectImage} 
+      <Proyect proy={"https://i.ibb.co/0nYffsW/Agronix-app.jpg"} 
       titleProy='Agronix S.R.L'
       descProy={`En desarrollo, Página web estática de presentación de empresa Agronix S.R.L dedicada a venta de insumos tecnológicos para el campo. Html, Css y Javascript vanilla.`}
       linkProy='https://github.com/federicojoaquin/Agronix-S.R.L'
